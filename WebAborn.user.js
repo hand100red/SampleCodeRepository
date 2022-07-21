9ce47dc6f2eeec83bad7dae91718da5e699cabc7
hand100red <hand100red@gmail.com>
2022-07-22 02:22:20 +0900

    Update WebAborn.user.js

--- WebAborn.user.js
+++ WebAborn.user.js
@@ -1,13 +1,29 @@
 // ==UserScript==
+// @name           WebAborn_hand100red
+// @version        1.0
+// @namespace      https://github.com/hand100red/SampleCodeRepository/blob/master/WebAborn.user.js
+// @description    Reduce the situation you see disagreeable texts in the way replacing to some word.  ('aborn' means 'purge and unable to read'.)
+// @author         hand100red
+// @grant          none
+// @include        *
+// ==/UserScript==
+(function () {
+/*
+  //このファイルの内容をGreasemonkeyで作成したユーザースクリプトに貼り付けることで使用できます。
+  //You can use it by pasting the contents of this file into a user script created with Greasemonkey.
+  //右記のサイトで作成した元々の内容は以下のとおりです。URL: "https://webaborn.herokuapp.com/"
+  //The original contents created on the site on the right are listed below. URL: "https://webaborn.herokuapp.com/"
 // @name           WebAborn
-// @version        19.20220629035616
+// @version        19.20220629102316
 // @namespace      http://webaborn.herokuapp.com
 // @description    Reduce the situation you see disagreeable texts in the way replacing to some word.  ('aborn' means 'purge and unable to read'.)
-// @include        *
-// ==/UserScript
-(function () {
+*/
   const abornString = 'あぼ～ん';
-  const ng_words = ['\u751f','\u6b7b']; //■NGワードの作成はこちらから→■https://webaborn.herokuapp.com/
+  //以下は\uで始まるユニバーサル文字で設定されていますが、日本語や英語をそのまま設定しても動作します。
+  //The following is set with universal characters starting with \ u, but it will work even if Japanese or English is set as it is.
+  //動作しない場合は、改行文字\u000D\u000Aが含まれていないことを確認してください。
+  //If it doesn't work, make sure the the newline character \u000D\u000A is not included.
+  const ng_words = ['\u751f','\u6b7b']; //←左記は例です。 <- The left is an example.
   var webaborn = function(node){
     var candidates = document.evaluate('.//text()[not(parent::style) and not(parent::textarea) and not(parent::script)]', node, null, 6, null);
     var i, j, lenC, lenNG, txt;
